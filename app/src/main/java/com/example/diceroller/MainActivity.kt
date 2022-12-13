898bae04ac79fb2e01ac4460b7eea955c26f431d
coderSitun <ksaswat007@gmail.com>
2022-12-13 13:20:22 +0530

    Added dice roll logic in MainActivity

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice(){
+        val result : TextView = findViewById(R.id.textView)
+        result.text = Dice(6).roll().toString()
     }
 }
